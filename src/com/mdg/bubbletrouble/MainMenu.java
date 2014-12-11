9fa75052cadc889cc9c88120ae6c3da17a1eeab1
deep-154 <deepankar11093@gmail.com>
2014-12-12 00:11:31 +0530

    options dialog added

--- src/com/mdg/bubbletrouble/MainMenu.java
+++ src/com/mdg/bubbletrouble/MainMenu.java
@@ -27,12 +27,20 @@ public class MainMenu extends Activity{
 		r = (RelativeLayout) findViewById(R.id.background);
 		
 		ImageButton play = new ImageButton(this);
-        play.setBackgroundColor(color.transparent);
+        play.setBackgroundColor(Color.TRANSPARENT);
 		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(10*width/37,42*height/100);
 		params.leftMargin = 70*width/100;
 		params.topMargin = height/23;
 		
+		ImageButton options = new ImageButton(this);
+        options.setBackgroundColor(Color.TRANSPARENT);
+		RelativeLayout.LayoutParams paramsOpt = new RelativeLayout.LayoutParams(18*width/100,26*height/100);
+		paramsOpt.leftMargin = 54*width/100;
+		paramsOpt.topMargin = 43*height/100;
+		
 		r.addView(play, params);
+		r.addView(options, paramsOpt);
+		
 		
 		play.setOnClickListener(new View.OnClickListener() {
 			
@@ -43,6 +51,18 @@ public class MainMenu extends Activity{
 				startActivity(i);
 			}
 		});
+		
+		options.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Dialog d = new Dialog(MainMenu.this);
+				 d.setTitle("Options");
+				 d.setContentView(R.layout.options);
+				 d.show();
+			  
+			}
+		});
 	}
-
 }
